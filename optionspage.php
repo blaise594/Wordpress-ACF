be2cf82286772a3a24e2efa3acf79189a2021ff0
blaise594 <danielbrogers@gmail.com>
2018-10-05 13:37:45 -0400

    first commit

---
+++ optionspage.php
@@ -0,0 +1,46 @@
+<?php
+//Insert into functions.php file
+/*********************
+ADD ACF OPTION PAGES
+add the acf options tab to the admin navigation.
+*********************/
+//add the acf options tab to the admin navigation.
+if(function_exists('acf_add_options_page')) { 
+
+    acf_add_options_page();
+    acf_add_options_sub_page('Header');
+    acf_add_options_sub_page('Footer');
+    //General Theme Options
+    acf_add_options_page(array(
+        'page_title'    => 'Website Options',
+        'menu_title'    => 'Website Options',
+        'menu_slug'     => 'website-options',
+        'capability'    => 'edit_posts',
+        'parent_slug'   => '',
+        'position'      => false,
+        'icon_url'      => false,
+    ));
+    
+    acf_add_options_sub_page(array(
+        'page_title'    => 'Header',
+        'menu_title'    => 'Header',
+        'menu_slug'     => 'website-options-header',
+        'capability'    => 'edit_posts',
+        'parent_slug'   => 'website-options',
+        'position'      => false,
+        'icon_url'      => false,
+    ));
+
+    acf_add_options_sub_page(array(
+        'page_title'    => 'Footer',
+        'menu_title'    => 'Footer',
+        'menu_slug'     => 'website-options-footer',
+        'capability'    => 'edit_posts',
+        'parent_slug'   => 'website-options',
+        'position'      => false,
+        'icon_url'      => false,
+    ));
+
+    
+}
+?>
---
+++ postloop.php
@@ -0,0 +1,54 @@
+
+<?php 
+
+function get_the_user_ip() {
+    if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+    //Checks if IP is from shared internet
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+    //Checks if IP is passed from proxy
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } 
+    else {
+    //Most trustworthy source of IP address
+    $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    //Uses ipinfo.io to find location information based on IP address
+    $details = json_decode(file_get_contents("https://ipinfo.io/{$ip}"));
+
+    //Returns city value from the details array
+    $city=$details->city;
+
+    return apply_filters('wpb_get_ip', $city );
+    }
+
+
+$posts = get_posts(array(
+	'posts_per_page'	=> -1,
+	'post_type'			=> 'location'
+));
+$userCity=get_the_user_ip();
+if( $posts ): 
+	
+	
+		
+	 foreach( $posts as $post ): 
+		
+        $cityField=get_field('city');
+        //echo "$cityField";        
+        $phoneField=get_field('phone_number');
+        //echo "$phoneField";
+         if($userCity==$cityField){
+             echo ($phoneField);
+         }
+	
+	 endforeach;
+	
+	
+	wp_reset_postdata(); 
+
+ endif; 
+
+//function display_phone(){
+?>
